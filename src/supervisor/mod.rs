8e65ae1f09790f81254b71abacf763de0929b1ab
PhotonQuantum <self@lightquantum.me>
2021-11-20 23:27:57 +0800

    feat: supervisor join

--- src/supervisor/mod.rs
+++ src/supervisor/mod.rs
@@ -1,13 +1,16 @@
 use actix::{Actor, Addr};
+use futures::future;
 
 use actor::ProgramActor;
 
 use crate::config::Program;
 use crate::logger::LoggerActor;
+use crate::supervisor::actor::Wait;
 
 mod actor;
 mod terminate_ext;
 
+#[derive(Debug)]
 pub struct Supervisor {
     actors: Vec<Addr<ProgramActor>>,
 }
@@ -23,5 +26,10 @@ impl Supervisor {
                 .map(|(name, program)| ProgramActor::new(name, program, logger.clone()).start())
                 .collect(),
         }
+    }
+    pub async fn join(self) {
+        future::try_join_all(self.actors.into_iter().map(|addr| addr.send(Wait)))
+            .await
+            .expect("join");
     }
 }
